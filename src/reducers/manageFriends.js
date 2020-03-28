4bf56c9920f94fdcf6011be818725e4eda2dc392
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2020-03-28 14:24:59 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
-export function manageFriends(state, action){
+export function manageFriends(state={friends: []}, action){
+    switch (action.type) {
+        case 'ADD_FRIEND':
+                return {...state,friends: [...state.friends, action.friend]}
+            break;
+        case 'REMOVE_FRIEND':
+            let index = state.friends.findIndex(f => f.id === action.id)
+            let newArr = [...state.friends.slice(0, index), ...state.friends.slice(index + 1)]
+            return {...state,friends: newArr}
+            break;
+    
+        default:
+            return state;
+    }
 }
